70943979398cf54bcd63da48b0dc56b81c5d5be7
HU-M <83540224+HU-M@users.noreply.github.com>
2021-12-09 13:11:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+status ="";
+video = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(480,300);
+    canvas.center();
+    
+}
+
+function draw() {
+    image(video, 0, 0, 400, 300);
+    if(status != "")
+    {
+        objectDetector.detect(video,gotResult);
+        for(i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects found";
+            document.getElementById("number_of_objects").innerHTML =  "Number of objects founds are : "+ objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    } 
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+
+}
